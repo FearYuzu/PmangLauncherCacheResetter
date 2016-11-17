6809530673976cc00624a1867d9b837190e5a9a3
S.Yamada (a.k.a Feartime) <administrator@indigoflare.net>
2016-11-17 15:27:00 +0900

    Add files via upload

---
+++ PmangLauncherCacheResetter/Confirm.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.IO;
+
+namespace PmangLauncherCacheResetter
+{
+    /// <summary>
+    /// Confirm.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Confirm : Window
+    {
+        public Confirm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartCacheDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var mw = new MainWindow();
+            string[] AllCache = Directory.GetFiles(MainWindow.CachePathOrigin);
+            foreach (string cachefile in AllCache)
+            {
+                File.SetAttributes(cachefile, FileAttributes.Normal);
+                File.Delete(cachefile);
+            }
+            MessageBox.Show("正常にキャッシュファイルを削除しました。", "削除完了", MessageBoxButton.OK, MessageBoxImage.Information);
+            GetCacheName();
+            
+        }
+
+        private void DeleteSetectedCache_Click(object sender, RoutedEventArgs e)
+        {
+            var mw = new MainWindow();
+            try
+            {
+                File.Delete(CacheList.SelectedItem.ToString());
+                MessageBox.Show("正常にキャッシュファイルを削除しました。", "削除完了", MessageBoxButton.OK, MessageBoxImage.Information);
+                
+                GetCacheName();
+                
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("キャッシュファイルが指定されていません。", "未指定", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                GetCacheName();
+            }
+            
+        }
+
+        private void ListUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            GetCacheName();
+            
+        }
+        public void GetCacheName()
+        {
+            CacheList.Items.Clear();
+            IEnumerable<string> files = Directory.EnumerateFiles(MainWindow.CachePathOrigin, "*", SearchOption.AllDirectories);
+            foreach (string f in files)
+            {
+                CacheList.Items.Add(f);
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+    
+}
---
+++ PmangLauncherCacheResetter/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using Microsoft.Win32;
+using System.IO;
+using System.ComponentModel;
+
+namespace PmangLauncherCacheResetter
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow :  Window
+    {
+        public static string CachePathOrigin;
+        public static string CachePath;
+        Confirm confirm = new Confirm();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CachePathOrigin = gameInstallPath() + ".gameon\\";
+            CachePath = CachePathOrigin.Replace("BlackDesert_live", "BlackDesert__live");
+            ClientPath.Content = "キャッシュパス　:　" + CachePath;
+            GetCacheName();
+            Closed += new System.EventHandler(Closing);
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            confirm.Show();
+            
+
+        }
+        private string gameInstallPath()
+        {
+            string keyName = string.Format("HKEY_CURRENT_USER\\SOFTWARE\\GameOn\\Pmang\\BlackDesert_live\\");
+            return (string)Registry.GetValue(keyName, "location", "");
+        }
+        public void GetCacheName()
+        {
+            //MessageBox.Show("aaa");
+            IEnumerable<string> files = Directory.EnumerateFiles(CachePathOrigin, "*", SearchOption.AllDirectories);
+            foreach (string f in files)
+            {
+                confirm.CacheList.Items.Add(f);
+                
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void Closing(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
